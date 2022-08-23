bbb83337235826343fe62b17c10ed15d70fe10aa
GitShaAli <shafeeqalibn@outlook.com>
2022-08-23 14:28:32 +0530

    AutoWriterText

---
+++ AutoTextWriter/script.js
@@ -0,0 +1,11 @@
+const write = "Hello.. User..! "
+let index = 1;
+function writer(){
+    document.body.innerText=write.slice(0,index);
+    index++;
+    if(index>write.length){
+        index=0;
+    }
+}
+
+setInterval(writer,100);
