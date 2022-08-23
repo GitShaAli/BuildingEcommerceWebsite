5aad0e41ff443af787186d90ac1786accf9c4712
GitShaAli <shafeeqalibn@outlook.com>
2022-08-23 14:10:48 +0530

    ToastMessageNotification.

---
+++ toastNotification/script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('btn');
+const container = document.getElementById('container');
+
+btn.addEventListener('click',()=>{
+    createNotification();
+})
+
+function createNotification(){
+    const notification = document.createElement('div');
+    notification .classList.add('toast');
+
+    notification.innerText = "Notification.. Display!";
+
+    container.appendChild(notification );
+
+    setTimeout(() => {
+        notification .remove();
+    }, 3000);
+
+}
